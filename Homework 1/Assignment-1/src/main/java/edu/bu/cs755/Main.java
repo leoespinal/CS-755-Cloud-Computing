a8f610976dec92f9f7a7ca4bd38d51cdd3ba9c77
Leo Espinal <leo.espinal420@gmail.com>
2018-02-10 18:05:48 -0500

    Working version of first part of assignment 1

--- Homework 1/Assignment-1/src/main/java/edu/bu/cs755/Main.java
+++ Homework 1/Assignment-1/src/main/java/edu/bu/cs755/Main.java
@@ -4,13 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 import java.util.AbstractMap.SimpleEntry;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
-
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 import org.apache.log4j.PropertyConfigurator;
@@ -22,15 +20,6 @@ import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 public class Main {
-	
-	
-	 public static String extractText(String line){
-	        
-		 
-		 
-		 return "";
-	 }
-
 	public static void main(String[] args) {
 
 		// Configure the log4j
@@ -47,42 +36,42 @@ public class Main {
 		try {
 			S3Object s3object = s3Client.getObject(bucket_name, key_name);
 			S3ObjectInputStream s3is = s3object.getObjectContent();
-			
-// Some s3 file metadata printouts 			
-//		    System.out.println(s3object.getObjectMetadata().getContentType());
-//		    System.out.println(s3object.getObjectMetadata().getContentLength());
 
 		    BufferedReader reader = new BufferedReader(new InputStreamReader(s3object.getObjectContent()));
-		    
-//		    Stream<String> lines=reader.lines().parallel();
-//		    System.out.println("Number of Lines: "+ lines.count());
-//		    lines.forEach(System.out::println);
-		    		
-		
-		    
-		    Map<String, Integer> wordCount=reader.lines().parallel()
-		    		.map(line -> line.split(">")[1].replaceAll("<[^>]+>", "")) // 
+
+            //Find count of each word instance
+		    Map<String, Integer> wordCount = reader.lines().parallel()
+		    		.map(line -> line.split(">")[1].replaceAll("<[^>]+>", "")) //
 		    		.flatMap(line -> Arrays.stream(line.trim().split(" ")))
-		    		.map(word -> word.replaceAll("[^a-zA-Z]", "").toLowerCase().trim())
+		    		.map(word -> word.replaceAll("[^a-zA-Z]", "").toUpperCase().trim())
 		    		.filter(word -> word.length() > 0)
 		            .map(word -> new SimpleEntry<>(word, 1)) // create a tuple of (word, 1)
-		            .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (v1, v2) -> v1 + v2)); // do the actual reduce by adding values with the same keys 
-		    
-		    
-		    
-		    // Now, we want to sort and get the top 50 values. 
-		    Map<String, Integer> result = wordCount.entrySet().parallelStream()
-		    			.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-		    			.limit(50) // limit the output to 50 elements
-		    			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-		    
-		    
-		    result.forEach((k, v) -> System.out.println(String.format("%s ->  %d", k, v)));
-		    
-		    System.out.println(result.keySet());
+					.collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (v1, v2) -> v1 + v2)); // do the actual reduce by adding values with the same keys
 
-		    
-		    
+            //Find most frequent 5000 words sorted most frequent to least frequent
+			List<String> topWords = wordCount.entrySet().parallelStream()
+					.sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+					.map(word -> word.getKey())
+					.limit(5000)
+					.collect(Collectors.toList());
+
+            //Target words
+            List<String> targetWords = Arrays.asList("during", "and", "time", "protein", "car").parallelStream().map(w -> w.toUpperCase()).collect(Collectors.toList());
+
+            System.out.println("Scanning top words list for target words...");
+            for (String element:targetWords) {
+                if(topWords.contains(element)) {
+                    int index = topWords.indexOf(element);
+                    String word = topWords.get(index);
+                    System.out.println(String.format("Index: [%d] -> Word: %s", index, word));
+                }
+                else {
+                    //Word is not found in dictionary of top words
+                    int indexNotFound = -1;
+                    System.out.println(String.format("Index: [%d] -> Word: %s", indexNotFound, element));
+                }
+            }
+
 			s3is.close();
 			
 		} catch (AmazonServiceException e) {
